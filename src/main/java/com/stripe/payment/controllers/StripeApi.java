fbae8cd26ef2839b60d441296b57e1b6602748c8
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-02 17:45:11 -0500

    Implemented logic behind StripeApi

--- src/main/java/com/stripe/payment/controllers/StripeApi.java
+++ src/main/java/com/stripe/payment/controllers/StripeApi.java
@@ -1,4 +1,17 @@
 package com.stripe.payment.controllers;
 
+import com.stripe.payment.common.constants.ApiPathConstants;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(ApiPathConstants.V1_ROUTE + ApiPathConstants.STRIPE_ROUTE)
 public interface StripeApi {
+
+    @PostMapping("/webhook")
+    ResponseEntity<Void> stripeWebhook(
+            @RequestBody String payload,
+            @RequestHeader("Stripe-signature") String stripeHeader);
 }

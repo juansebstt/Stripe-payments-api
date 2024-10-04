6bbe96a04c37f2e943fa551ccbb5f2048e36b054
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-04 17:01:36 -0500

    Implemented some of the Strategy service with Design Pattern Build()

---
+++ src/main/java/com/stripe/payment/strategy/impl/StripeStrategyPaymentIntentSucceed.java
@@ -0,0 +1,41 @@
+package com.stripe.payment.strategy.impl;
+
+import com.stripe.model.Event;
+import com.stripe.model.PaymentIntent;
+import com.stripe.payment.common.entities.Payment;
+import com.stripe.payment.common.enums.StripeEventEnum;
+import com.stripe.payment.strategy.StripeStrategy;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class StripeStrategyPaymentIntentSucceed implements StripeStrategy {
+    @Override
+    public boolean isApplicable(Event event) {
+        return StripeEventEnum.PAYMENT_EVENT_SUCCEED.value.equals(event.getType());
+    }
+
+    @Override
+    public Event process(Event event) {
+        return Optional.of(event)
+                .map(this::deserialize)
+                .map(this::mapToEntity)
+                .orElseThrow(() -> new RuntimeException("Error processing event"));
+    }
+
+    private Payment mapToEntity(PaymentIntent paymentIntent) {
+        return  Payment.builder()
+                .paymentIntentId(paymentIntent.getId())
+                .customerId(paymentIntent.getCustomer())
+                .amount(paymentIntent.getAmount())
+                .currency(paymentIntent.getCurrency())
+                .type(StripeEventEnum.PAYMENT_EVENT_SUCCEED)
+                .build();
+    }
+
+    private PaymentIntent deserialize(Event event) {
+        return (PaymentIntent) event.getDataObjectDeserializer().getObject()
+                .orElseThrow(() -> new RuntimeException("Error deserializing event"));
+    }
+}
